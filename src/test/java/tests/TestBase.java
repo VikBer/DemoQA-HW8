ced5800c08ddc12dcd197698e396b9a2c449978a
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2024-01-23 22:19:14 +0300

    fina2l

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = System.getProperty("1920x1080");
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
 
